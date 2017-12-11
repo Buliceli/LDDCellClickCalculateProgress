174dcaf2ea98b470b9bc222e24053251fdc1ce85
Longma <zhangkai031092@163.com>
2017-12-11 10:10:31 +0800

    测试数据

---
+++ cellSelectedCalculateProgress/cellSelectedCalculateProgress/SixModel.h
@@ -0,0 +1,16 @@
+//
+//  SixModel.h
+//  cellSelectedCalculateProgress
+//
+//  Created by Longma on 17/12/11.
+//  Copyright © 2017年 Minte. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+/*
+测试数据
+ */
+@interface SixModel : NSObject
+
+@end
---
+++ cellSelectedCalculateProgress/cellSelectedCalculateProgress/SixModel.m
@@ -0,0 +1,13 @@
+//
+//  SixModel.m
+//  cellSelectedCalculateProgress
+//
+//  Created by Longma on 17/12/11.
+//  Copyright © 2017年 Minte. All rights reserved.
+//
+
+#import "SixModel.h"
+
+@implementation SixModel
+
+@end
